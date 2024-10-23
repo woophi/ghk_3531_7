fe0262171b20e3ba7be60e6476a090051ba1e0f4
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-10-23 10:33:08 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ export const App = () => {
           <div
             className={appSt.gridItem}
             onClick={() => {
-              window.gtag('event', 'ISG_7');
+              window.gtag('event', 'ISG_3531_7');
               setSelected('isz');
               setOpenBs(true);
             }}
@@ -66,7 +66,7 @@ export const App = () => {
 
           <div
             onClick={() => {
-              window.gtag('event', 'NSG_7');
+              window.gtag('event', 'NSG_3531_7');
               setSelected('nsz');
               setOpenBs(true);
             }}
@@ -87,7 +87,7 @@ export const App = () => {
           <a
             className={appSt.gridItem}
             href="alfabank://webFeature?type=recommendation&url=https%3A%2F%2Fdfa.alfabank.ru%2Fretail-investor%2Fwebview%2F%3Fchannel%3Dghk3531"
-            onClick={() => window.gtag('event', 'DFA_7')}
+            onClick={() => window.gtag('event', 'DFA_3531_7')}
           >
             <div>
               <Typography.Text view="primary-medium" tag="p" defaultMargins={false} weight="medium">
@@ -103,7 +103,7 @@ export const App = () => {
           <a
             className={appSt.gridItem}
             href="alfabank://investments/catalogue?tab=MUTUAL"
-            onClick={() => window.gtag('event', 'Fonds_7')}
+            onClick={() => window.gtag('event', 'Fonds_3531_7')}
           >
             <div>
               <Typography.Text view="primary-medium" tag="p" defaultMargins={false} weight="medium">
@@ -119,7 +119,7 @@ export const App = () => {
           <a
             className={appSt.gridItem}
             href="alfabank://investments/catalogue?tab=TRUST"
-            onClick={() => window.gtag('event', 'DU_7')}
+            onClick={() => window.gtag('event', 'DU_3531_7')}
           >
             <div>
               <Typography.Text view="primary-medium" tag="p" defaultMargins={false} weight="medium">
@@ -137,7 +137,12 @@ export const App = () => {
       <Gap size={96} />
 
       <div className={appSt.bottomBtn}>
-        <ButtonMobile block view="primary" href="alfabank://investments" onClick={() => window.gtag('event', 'Vitrina_7')}>
+        <ButtonMobile
+          block
+          view="primary"
+          href="alfabank://investments"
+          onClick={() => window.gtag('event', 'Vitrina_3531_7')}
+        >
           Витрина инвестиций
         </ButtonMobile>
       </div>
@@ -146,9 +151,9 @@ export const App = () => {
         open={openBs}
         onClose={() => {
           if (selectedItem === 'isz') {
-            window.gtag('event', 'ISG_Exit_7');
+            window.gtag('event', 'ISG_3531_Exit_7');
           } else {
-            window.gtag('event', 'NSG_Exit_7');
+            window.gtag('event', 'NSG_3531_Exit_7');
           }
           setOpenBs(false);
         }}
@@ -166,9 +171,9 @@ export const App = () => {
             }
             onClick={() => {
               if (selectedItem === 'isz') {
-                window.gtag('event', 'ISG_More_info_7');
+                window.gtag('event', 'ISG_3531_More_info_7');
               } else {
-                window.gtag('event', 'NSG_More_info_7');
+                window.gtag('event', 'NSG_3531_More_info_7');
               }
             }}
           >
@@ -192,9 +197,9 @@ export const App = () => {
             weight="regular"
             onClick={() => {
               if (selectedItem === 'isz') {
-                window.gtag('event', 'ISG_Mobile_7');
+                window.gtag('event', 'ISG_3531_Mobile_7');
               } else {
-                window.gtag('event', 'NSG_Mobile_7');
+                window.gtag('event', 'NSG_3531_Mobile_7');
               }
 
               copy('+7 495 788-88-78');
